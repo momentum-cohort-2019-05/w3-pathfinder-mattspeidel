48ea257c6f87e3ff5f5c66b4265e430a8161f3f0
mattspeidel <mattspeidel@gmail.com>
2019-06-15 20:09:23 -0400

    add Draw class, define methods, cleanup to accomodate OOP, add some commenting

--- pathfinder.py
+++ pathfinder.py
@@ -7,13 +7,9 @@ def assign_color_value(elevation):
     assigned_color = (elevation // 10) - 310
     return assigned_color
 
-with open('elevation_small.txt') as file:
-    llist = [[int(num) for num in line.split()] for line in file]
-
-y = 0
-x = 0
-
 def choose_closest_elevation(x, y):
+    """chooses closest elevation change block and stores the amount of change for use
+    in determining the ideal path"""
     while True:  
         try:
             top = llist[y - 1][x + 1]
@@ -50,47 +46,63 @@ def choose_closest_elevation(x, y):
             output = [1, -1, abs(llist[y][x]-closest)]
             return output
 
+class Draw:
+    """Drawing object contains different drawing methods"""
+    def __init__(self):
+        self.y = 0
+        self.x = 0
+
+    def mapdraw(self):
+        while self.y < 600:
+            """prints map"""
+            while self.x < 600:
+                """prints each line of map"""
+                emap.putpixel((self.x, self.y), (assign_color_value(llist[self.y][self.x]), assign_color_value(llist[self.y][self.x]), assign_color_value(llist[self.y][self.x])))
+                self.x += 1
+
+            self.y += 1
+            self.x = 0
+
+    def pathdraw(self):
+        self.y = 0
+        self.origin = 0
+        self.elevation_change_total = 0
+        self.prev_total = 99999
+        self.shortest_origin = 0
+        emap.putpixel((self.x, self.y), (255, 0, 0))
+
+        while self.origin < 600:
+            """prints all the paths"""
+            while self.x < 599:
+                """prints individual paths and chooses route"""
+                self.output = choose_closest_elevation(self.x, self.y)
+                self.x += self.output[0]
+                self.y += self.output[1]
+                self.elevation_change_total += self.output[2]
+                emap.putpixel((self.x, self.y), (255, 0, 0))
+            if self.elevation_change_total < self.prev_total:
+                self.prev_total = self.elevation_change_total
+                self.shortest_origin = self.origin
+            self.elevation_change_total = 0
+            self.origin += 1
+            self.y = self.origin
+            self.x = 0
+
+    def idealdraw(self):
+        self.y = self.shortest_origin
+        while self.x < 599:
+            """prints the ideal path"""
+            self.output = choose_closest_elevation(self.x, self.y)
+            self.x += self.output[0]
+            self.y += self.output[1]
+            emap.putpixel((self.x, self.y), (255, 255, 0))
+
+with open('elevation_small.txt') as file:
+    llist = [[int(num) for num in line.split()] for line in file]
+
 emap = Image.new('RGBA', (600, 600))
-
-while y < 600:
-    """prints map"""
-    while x < 600:
-        """prints each line of map"""
-        emap.putpixel((x, y), (assign_color_value(llist[y][x]), assign_color_value(llist[y][x]), assign_color_value(llist[y][x])))
-        x += 1
-
-    y += 1
-    x = 0
-
-y = 0
-origin = 0
-elevation_change_total = 0
-prev_total = 99999
-shortest_origin = 0
-emap.putpixel((x, y), (255, 0, 0))
-
-while origin < 600:
-    """prints all the paths"""
-    while x < 599:
-        """prints individual paths and chooses route"""
-        output = choose_closest_elevation(x, y)
-        x += output[0]
-        y += output[1]
-        elevation_change_total += output[2]
-        emap.putpixel((x, y), (255, 0, 0))
-    if elevation_change_total < prev_total:
-        prev_total = elevation_change_total
-        shortest_origin = origin
-    elevation_change_total = 0
-    origin += 1
-    y = origin
-    x = 0
-
-y = shortest_origin
-while x < 599:
-    output = choose_closest_elevation(x, y)
-    x += output[0]
-    y += output[1]
-    emap.putpixel((x, y), (255, 255, 0))
-
+d = Draw()
+d.mapdraw()
+d.pathdraw()
+d.idealdraw()
 emap.save('map.png')
